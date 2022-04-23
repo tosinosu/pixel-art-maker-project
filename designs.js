87baca9f9d6853efed189ad590aacb424cc6915b
Osunwale Tosin <tosinosu@gmail.com>
2022-04-24 00:17:12 +0100

    Height and width positions corrected

--- designs.js
+++ designs.js
@@ -1,40 +1,58 @@
-// Select color input and parameters
 
-
+// Select size input
 const cellHeight = document.getElementById('inputHeight');
 const cellWidth = document.getElementById('inputWidth');
+
+// Select color input
 const color  = document.getElementById('colorPicker');
+
+// Select button 
 const sizePicker = document.getElementById("sizePicker");
+
+// Select table position
 const table = document.getElementById('pixelCanvas');
 
 
-//submit button is clicked
+// When size is submitted by the user, call makeGrid()
 sizePicker.addEventListener("submit", function(event){
     event.preventDefault();
     makeGrid(cellHeight, cellWidth, color, table);
 });
    
     
-// When size is submitted by the user, call makeGrid()
-function makeGrid(height, width, color, table) {
+/**
+ * Makes a colored grid based on inserted size and color.
+ *
+ * @param {number} height The height of the grid, based on inserted number.
+ * @param {number} width The width of the grid, based on inserted number.
+ * @param {number} color Fills the grid based on the collor selected.
+ * @param {number} table Get the pixelCanvas from the index.html.
+ */
 
+function makeGrid(height, width, color, table) {
+    // Select random colors from the array of colors provided
     let randomBorderArray = ["blue", "green" ,"yellow", "red", "black"];
     let randomBorderColor = randomBorderArray[Math.floor(Math.random() * randomBorderArray.length)];
 
+    // Deletes the previous table when Makegrid is called 
     while (table.firstChild){
         table.removeChild(table.firstChild);
     }
 
-    for (let row  = 0; row < width.value; row++){
+    // Builds the grids using the inserted height and width
+    for (let row  = 0; row < height.value; row++){
         let cellRow = table.insertRow(row);
-        for(let col = 0; col < height.value; col++){
+    
+        for(let col = 0; col < width.value; col++){
             let cellColumn = cellRow.insertCell(col);
+            
+            // Changes border color
+            cellColumn.style.borderColor = randomBorderColor;
 
-            cellColumn.style.border = '1px solid '+ randomBorderColor;
-
+            // Adds selected colors to each selected cell
             cellColumn.addEventListener("click", function() {
                 this.style.backgroundColor = color.value;
             });
         }
-    }
+    };
 }
